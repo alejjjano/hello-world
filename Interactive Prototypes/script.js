ad9c57fa2a0684fd8a5a1163d43fc2595a0fdf6d
DTORERO <dtorero@minedu.gob.pe>
2018-02-01 14:36:23 -0500

    Added js functions and css styling. button replaced to <button> tag

--- Interactive Prototypes/script.js
+++ Interactive Prototypes/script.js
@@ -3,5 +3,16 @@ btn = document.getElementById("btn");
 toWrite = document.getElementById("intro");
 
 btn.onclick = function(){
-    toWrite.innerHTML = text.value;
+    toWrite.innerHTML +=  "<br>" + text.value;
+    text.value = "";
 }
+
+// I don't fully understand this function
+// should do more reaserch about event listeners
+
+document.getElementById("text").addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("btn").click();
+    }
+});
